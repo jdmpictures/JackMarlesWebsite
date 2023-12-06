60d189e48f7fb561c484ce6568bd9229d0d452b7
Jack Marles <jdmpictures@gmail.com>
2023-12-06 23:54:22 +0000

    Made header responsive

--- dist/js/about.js
+++ dist/js/about.js
@@ -1,7 +1,7 @@
 function setDate() {
     const currentDate = new Date();
     const day = currentDate.toLocaleString('en-us', { weekday: 'short' }); // Get the first three letters of the day
-    const dayDigit = currentDate.getDay();
+    const dayDigit = currentDate.getDate();
     const month = currentDate.toLocaleString('en-us', { month: 'short' }); // Get the first three letters of the month
     const year = currentDate.getFullYear();
     const formattedDate = `${day} ${dayDigit} ${month}`;
